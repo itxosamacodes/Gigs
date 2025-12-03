0a6e0eb634996b8ae7d0051b301d5663339c3519
itxosamacodes <itxosama.io@gmail.com>
2025-12-03 16:39:48 +0500

    start with clean

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
